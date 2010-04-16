79c038bb752af1da793d7ffa0b2568311ac296a9
kegorof <kegorof@1019b9a1-de4b-0410-b3bf-5fddcd4fe59e>
2010-04-16 08:39:40 +0000

    verify bankomat model

---
+++ test/java/ru/ifmo/test/bank/BankControlledObject.java
@@ -0,0 +1,52 @@
+/* 
+ * Developed by eVelopers Corporation, 2010
+ */
+package ru.ifmo.test.bank;
+
+import com.evelopers.unimod.runtime.context.StateMachineContext;
+
+/**
+ * @author kegorov
+ *         Date: Apr 15, 2010
+ */
+public class BankControlledObject {
+
+    public void z1(StateMachineContext c) {
+    }
+
+    public void z2(StateMachineContext c) {
+    }
+
+    public void z3(StateMachineContext c) {
+    }
+
+    public void z4(StateMachineContext c) {
+    }
+
+    public void z5(StateMachineContext c) {
+    }
+
+    public void z6(StateMachineContext c) {
+    }
+
+    public void z7(StateMachineContext c) {
+    }
+
+    public void z8(StateMachineContext c) {
+    }
+
+    public void z9(StateMachineContext c) {
+    }
+
+    public void z10(StateMachineContext c) {
+    }
+
+    public void z11(StateMachineContext c) {
+    }
+
+    public void z12(StateMachineContext c) {
+    }
+
+    public void z13(StateMachineContext c) {
+    }
+}
