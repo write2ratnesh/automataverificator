e2e9082017e534c8c285dd9d3e58faa38330a277
kegorof <kegorof@1019b9a1-de4b-0410-b3bf-5fddcd4fe59e>
2013-07-11 10:28:54 +0000

    nasa rule verification test (2)

--- trunk/test/java/ru/ifmo/test/nf2sl/NasaRuleEventProvider.java
+++ trunk/test/java/ru/ifmo/test/nf2sl/NasaRuleEventProvider.java
@@ -13,16 +13,18 @@ public class NasaRuleEventProvider {
      * nfv9 - nfv9/ipfix input template
      * tmpl - internal template <exp_ip, src_addr, dst_addr, src_port, dst_port...>
      * t - timer >= 60s
-     * nt - temer < 60s
      * c2_next - c2 has elements
      * c2_empty - c2 is empty
+     * e1 - c1 has next and exp == curExp
+     * e2 - c1 has next but exp != curExp
      */
 
     public static final String nfv5 = "nfv5";
     public static final String nfv9 = "nfv9";
     public static final String tmpl = "tmpl";
     public static final String t = "t";
-    public static final String nt = "nt";
+    public static final String e1 = "e1";
+    public static final String e2 = "e2";
     public static final String c2_next = "c2_next";
     public static final String c2_empty = "c2_empty";
 }
